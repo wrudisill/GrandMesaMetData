a4688cebdba1ac3ff58b04d7c1f5b557475815ad
bsu-wrudisill <williamrudisill@u.boisestate.edu>
2022-09-06 11:56:08 -0600

    add more description

--- process_data_initial.py
+++ process_data_initial.py
@@ -4,6 +4,12 @@ import pandas as pd
 import pathlib as pl
 
 
+## README -- the purpose of this script is to fix some weird formatting that was
+## in some, but not all, of the original .dat files
+## there were extra commas at the beginning of some lines, which messed up 
+## reading the data into pandas. i ran this script one-at-a time to 
+## fix the files that had this issue 
+
 
 raw_base = pl.Path("/Volumes/Transcend/snowex_raw_met_data/raw/10minute")
 processed_base = pl.Path("/Volumes/Transcend/snowex_raw_met_data/partially_processed")
@@ -15,14 +21,20 @@ list_of_newlines = []
 #dfile = raw_base.joinpath("GMSP2_Table1.dat")
 dfile = raw_base.joinpath("MW_Table1.dat")
 
+
+# you have to specify the encoding when opening
+# some of the files might have been utf-8 for some reason...
+# again i did this one file at a time since they were not all consistent 
 with open(dfile, encoding='utf-16') as f:
+    # loop through each line of the file and only keep the first to second to last lines
     for i in f:
     	list_of_newlines.append(i[1:-2])
 
-
+# open up a new file and write the corrected lines there 
 with open(processed_base.joinpath(dfile.name), encoding='utf-16', mode='w') as f:
     for i in list_of_newlines:
     	f.write(i)
+        # this is the newline character 
     	f.write("\n")
 
 
