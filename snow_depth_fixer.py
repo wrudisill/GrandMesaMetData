08b198d21349b052aef7ad0cf2839039ce41ebb7
bsu-wrudisill <williamrudisill@u.boisestate.edu>
2022-09-12 15:37:07 -0600

    add snowdepth calculation in

---
+++ snow_depth_fixer.py
@@ -0,0 +1,151 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import pathlib as pl
+import numpy as np
+import datetime
+
+
+def corr_dist(distance, temperature):
+	# temperature must be in celcius
+	return distance*np.sqrt((temperature + 273.15)/273.15)
+
+
+def wy(dt):
+	if dt.month in [10,11,12]:
+		wy = dt.year + 1
+	else:
+		wy = dt.year
+	return wy
+
+
+
+
+def snow_depth_fixer(data):
+
+	# this is the base data path...
+	processed_base = pl.Path("/Volumes/Transcend/snowex_raw_met_data/pandas_ready")
+
+	# these are the vars that we want...
+	snow_vars = ["TCDT", "TCDT_Avg", "TCDT_Max", "TCDT_Min", "TCDT_Std", "AirTC_10ft"]
+
+	# open up the dataframe...
+	df = pd.read_csv(processed_base.joinpath(data)).iloc[:,1:]
+	df.TIMESTAMP = pd.to_datetime(df.TIMESTAMP)
+	df = df.set_index("TIMESTAMP")
+
+	# check that the vars are in fact in there...
+	for c in snow_vars:
+		if c not in df.columns:
+			print(c + " NOT FOUND. Skipping")
+			print(data)
+		continue
+
+
+	# get all of the vars we might need...
+	dfsd = df[snow_vars]
+
+	# remove duplicate dates
+	dfsd = dfsd.reset_index().drop_duplicates("TIMESTAMP", keep="last").set_index("TIMESTAMP")
+
+	# make the index of the correct dates
+	the_right_dates       = pd.date_range(dfsd.index[0], dfsd.index[-1], freq='10min')
+
+	# reindex. fill with nans where missing
+	dfsd = dfsd.reindex(the_right_dates)
+
+	# # convert things to numeirc...
+	dfsd = dfsd.apply(pd.to_numeric, errors='coerce')
+
+	# apply the temp correction
+	dfsd['TCDT_Avg'] = corr_dist(dfsd['TCDT_Avg'], dfsd['AirTC_10ft'])
+
+	# get the differential
+	sdavg_diff = dfsd.TCDT_Avg.diff()
+
+	# remove the big jums. 10min change cant be less than/greather than .15 m
+	dfsd = dfsd.where(abs(sdavg_diff) < .15)
+
+	# now do a std. dev filtering
+	dfsd = dfsd.where(dfsd.TCDT_Std < dfsd.TCDT_Std.mean()*1.2)
+
+	# remove values taht are close to zero, this messes up things down the road
+	tol = .001 #1mm
+	dfsd = dfsd.where(dfsd.TCDT_Avg > tol)
+
+	# add a water year column to the data
+	dfsd['wy'] = [wy(x) for x in dfsd.index]
+
+	# find the location of the maximum
+	max_date_list = []
+	max_list      = []
+
+	#dfsd_corrected = dfsd.TCDT_Avg.deepcopy()
+
+	# make a new column for this
+	dfsd['SnowDepth'] = None
+
+
+	# go thru unique years
+	for y in dfsd.wy.unique():
+		# get the weekly median
+		wekmed = dfsd[dfsd.wy == y].TCDT_Avg.resample("1w").median()
+
+		# find the location of hte max depth
+		max_date_list.append(wekmed.idxmax())
+
+		# do the same for the other one
+		max_list.append(wekmed.max())
+
+		# create the depth product... still needs to be resampled, etc.
+		dfsd['SnowDepth'][dfsd.wy == y] = wekmed.max() - dfsd[dfsd.wy == y].TCDT_Avg
+
+
+	# remove values that are very close to, or below, zero
+	tol = .001
+
+	# remove some more points..
+	SnowDepth = dfsd["SnowDepth"].where(dfsd.SnowDepth > 0)
+	SnowDepth = SnowDepth.astype(float)
+
+	# now do some gap filling ...
+	SnowDepth_interp = SnowDepth.interpolate()
+
+	# get the number of consecutive gaps in the data
+	# a bit ugly
+	# each col/row says the number of consectuve NA values for that variable
+	dfsdnull = dfsd.isnull()
+	gaps = dfsdnull.ne(dfsdnull.shift()).cumsum().apply(lambda x: x.map(x.value_counts())).where(dfsdnull)
+
+	# this is the maximum spacing that's allowed...
+	# this is equivalent to 3 hrs
+	max_gap = 18
+
+	# loop through all of the vaiables them..
+	SnowDepth_interp[gaps['SnowDepth']>max_gap] = np.NaN
+
+	# now resample the data...
+	hrly_sd = SnowDepth_interp.resample("1h").mean()
+
+	return hrly_sd
+
+
+# we do thi sjust so that this code doesnt get run on import
+# lets us use the above function in other scripts
+if __name__ == "__main__":
+
+	pass
+	# outname = "_".join(data.split("_")[0:2]) + "_snowd_plot.png"
+
+	# # make a plot
+	# fig,ax = plt.subplots()
+	# ax.plot(dfsd.index, dfsd.TCDT_Avg)
+	# ax.plot(hrly_sd.index, hrly_sd)
+	# plt.savefig(outname)
+	# plt.clf()
+
+
+
+
+
+
+
--- write_final_files.py
+++ write_final_files.py
@@ -5,23 +5,30 @@ import numpy as np
 from datetime import timedelta
 import sys
 
+# import some functions fomr scripts in the same directory that we're runnign things in
+# look at the fix_mm_lw.py script for this function
+from fix_mm_lw import fix_mm_lw, circular_mean
+from snow_depth_fixer import snow_depth_fixer
+
 
 ### This code reads the files that are written by the previous script,
 ### called "process_data_initial.py"
 
 
-## Purpose 
+## Purpose
 ## -------
-##  1) remove duplicate timesteps 
+##  1) remove duplicate timesteps
 ##  2) add missing timesteps and fill with NA value
-##  3) **interpolate between missing values**. only do this step for gaps 
-##     that are less than a certain lenght. the length is a variable that 
-##     that can be changed around. 
+##  3) **interpolate between missing values**. only do this step for gaps
+##     that are less than a certain lenght. the length is a variable that
+##     that can be changed around.
 ##  4) resample the data from 10 minute to hourly
-##  5) write a friendly and readable csv file 
+##  5) write a friendly and readable csv file
 
 # begin
 processed_base = pl.Path("/Volumes/Transcend/snowex_raw_met_data/pandas_ready")
+
+#datalist = ["GMSP1_Table1_pandas_happy.dat"]
 
 datalist  = ["MM_Table1_pandas_happy.dat",
              "LSOS_Table1_pandas_happy.dat",
@@ -36,73 +43,142 @@ datalist  = ["MM_Table1_pandas_happy.dat",
 for data in datalist:
 
 
-	# open up the dataframe...
-	df = pd.read_csv(processed_base.joinpath(data), low_memory=False).iloc[:,1:]
-	df.TIMESTAMP = pd.to_datetime(df.TIMESTAMP)
-	df = df.set_index("TIMESTAMP")
+    # open up the dataframe...
+    df = pd.read_csv(processed_base.joinpath(data), low_memory=False).iloc[:,1:]
+    df.TIMESTAMP = pd.to_datetime(df.TIMESTAMP)
+    df = df.set_index("TIMESTAMP")
 
-	# get just the vars that we want..
-	met_vars  = ["RH_10ft", "BP_kPa_Avg", "AirTC_20ft", "AirTC_10ft", "WSms_20ft", "WSms_10ft", "SUp_Avg", "SDn_Avg", "LUpCo_Avg", "LDnCo_Avg"]
-	dfmet = df[met_vars]
-	print("\n \n")
-	print("~~~Begin~~~")
-	print("Examining file ... %s"%data)
-	print("Total # of duplicated timestamps: %s"%dfmet.index.duplicated().sum())
-	print("Dropping duplicates... keeping first instance of each")
-
-	# drop duplicates method
-	#https://pandas.pydata.org/docs/reference/api/pandas.Index.drop_duplicates.html
-#	dfmet = dfmet.drop_duplicates()
-
-	# the above doesn't quite work... only works if the entire row is duplicated
-	# in some cases just the time is duplicated. ugh
-	dfmet = dfmet.reset_index().drop_duplicates("TIMESTAMP", keep="last").set_index("TIMESTAMP")
+    # get just the vars that we want..
+    met_vars  = ["RH_10ft", "RH_20ft", "BP_kPa_Avg",
+                 "AirTC_20ft", "AirTC_10ft",
+                 "WSms_20ft", "WSms_10ft",
+                 "WindDir_10ft_SD1_WVT", "WindDir_20ft_SD1_WVT",
+                 "SUp_Avg", "SDn_Avg",
+                 "LUpCo_Avg", "LDnCo_Avg",
+                 "SM_5cm_Avg", "SM_20cm_Avg", "SM_50cm_Avg"]
 
 
-	# now make a list of the dates that SHOULD be in there
-	the_right_dates = pd.date_range(dfmet.index[0], dfmet.index[-1], freq='10min')
+    dfmet = df[met_vars]
+    print("\n \n")
+    print("~~~Begin~~~")
+    print("Examining file ... %s"%data)
+    print("Total # of duplicated timestamps: %s"%dfmet.index.duplicated().sum())
+    print("Dropping duplicates... keeping first instance of each")
 
-	# missing dates
-	missing_times = (len(the_right_dates) - len(dfmet))
+    # drop duplicates method
+    #https://pandas.pydata.org/docs/reference/api/pandas.Index.drop_duplicates.html
+#   dfmet = dfmet.drop_duplicates()
 
-	# print out how many missing times there are
-	print("Total # of missing timesteps: %s"%missing_times)
+    # the above doesn't quite work... only works if the entire row is duplicated
+    # in some cases just the time is duplicated. ugh
+    dfmet = dfmet.reset_index().drop_duplicates("TIMESTAMP", keep="last").set_index("TIMESTAMP")
 
-	# "reindex" the data... this will add nans to spots where there is no data
-	dfmet = dfmet.reindex(the_right_dates)
 
-	# make the data numeric...
+    # now make a list of the dates that SHOULD be in there
+    the_right_dates = pd.date_range(dfmet.index[0], dfmet.index[-1], freq='10min')
+
+    # missing dates
+    missing_times = (len(the_right_dates) - len(dfmet))
+
+    # print out how many missing times there are
+    print("Total # of missing timesteps: %s"%missing_times)
+
+    # "reindex" the data... this will add nans to spots where there is no data
+    dfmet = dfmet.reindex(the_right_dates)
+
+    # make the data numeric...
         # this is because pandas treats the data as strings in some cases
-        # also converts strings of "NaN" to numpy NaN types 
-	dfmet = dfmet.apply(pd.to_numeric, errors='coerce')
-
-	# interpolate the data linearly
-	# later we will remove the values that have too-long of gaps
-	dfmet_interp = dfmet.interpolate()
-
-	# get the number of consecutive gaps in the data
-	# a bit ugly
-	# each col/row says the number of consectuve NA values for that variable
-	dmetnull = dfmet.isnull()
-	gaps = dmetnull.ne(dmetnull.shift()).cumsum().apply(lambda x: x.map(x.value_counts())).where(dmetnull)
-
-	# this is the maximum spacing that's allowed...
-	# this is equivalent to 3 hrs
-        max_gap = 18 
-	
-        # loop through all of the vaiables them..
-	for var in dfmet.columns:
-		print("%s: the maximum number of sequential timesteps missing is... %s"%(var, gaps[var].max()))
-		dfmet_interp[var][gaps[var]>max_gap] = np.NaN
-
-	# apply a mean filter to resample from 10min --> 1hr 
-	output_file = dfmet_interp.resample("1h").mean()
+        # also converts strings of "NaN" to numpy NaN types
+    dfmet = dfmet.apply(pd.to_numeric, errors='coerce')
 
 
-	# now save it
-	outname = "_".join(data.split("_")[0:2]) + "_final_output.csv"
-	output_file.to_csv(outname, index_label='TIMESTAMP')
+    # correct longwave...
+    if data == "MM_Table1_pandas_happy.dat":
 
+        # correct wierd lw data
+        lw_corrected = fix_mm_lw()
+        dfmet['LDnCo_Avg'][lw_corrected.index] = lw_corrected
+
+
+
+    # make new vars for the sin and cos parts of wind direction...
+    dfmet["WindDir_10ft_SD1_WVT_sin"] = np.sin(np.radians(dfmet["WindDir_10ft_SD1_WVT"]))
+    dfmet["WindDir_10ft_SD1_WVT_cos"] = np.cos(np.radians(dfmet["WindDir_10ft_SD1_WVT"]))
+    dfmet["WindDir_20ft_SD1_WVT_sin"] = np.sin(np.radians(dfmet["WindDir_20ft_SD1_WVT"]))
+    dfmet["WindDir_20ft_SD1_WVT_cos"] = np.cos(np.radians(dfmet["WindDir_20ft_SD1_WVT"]))
+
+    # interpolate the data linearly
+    # later we will remove the values that have too-long of gaps
+    dfmet_interp = dfmet.interpolate()
+
+    # get the number of consecutive gaps in the data
+    # a bit ugly
+    # each col/row says the number of consectuve NA values for that variable
+    dmetnull = dfmet.isnull()
+    gaps = dmetnull.ne(dmetnull.shift()).cumsum().apply(lambda x: x.map(x.value_counts())).where(dmetnull)
+
+    # this is the maximum spacing that's allowed...
+    # this is equivalent to 3 hrs
+    max_gap = 12
+
+    # loop through all of the vaiables them..
+    for var in dfmet.columns:
+        print("%s: the maximum number of sequential timesteps missing is... %s"%(var, gaps[var].max()))
+        dfmet_interp[var][gaps[var]>max_gap] = np.NaN
+
+
+    # get the variables that we apply the normal mean to...
+    #dfmet_regvars = dfmet[[x for x in dfmet.columns if x not in  ["WindDir_20ft_SD1_WVT", "WindDir_10ft_SD1_WVT"]]]
+
+    # apply a mean filter to resample from 10min --> 1hr
+    output_file = dfmet.resample("1h").mean()
+    output_file["WindDir_10ft_SD1_WVT"] = np.degrees(np.arctan2(output_file["WindDir_10ft_SD1_WVT_sin"],
+                                                                output_file["WindDir_10ft_SD1_WVT_cos"]))
+
+    output_file["WindDir_20ft_SD1_WVT"] = np.degrees(np.arctan2(output_file["WindDir_20ft_SD1_WVT_sin"],
+                                                                output_file["WindDir_20ft_SD1_WVT_cos"]))
+
+    output_file =  output_file.drop(["WindDir_20ft_SD1_WVT_sin",
+                                     "WindDir_20ft_SD1_WVT_cos",
+                                     "WindDir_10ft_SD1_WVT_sin",
+                                     "WindDir_10ft_SD1_WVT_cos"], axis=1)
+    # this isnot quite correct. we must apply a circular mean function to the wind dir
+
+
+    # DO THE SNOWDEPTH CALCULATION #
+    snow_depth = snow_depth_fixer(data)
+    output_file['SnowDepth(m)'] = snow_depth
+
+
+    ####################################
+    # APPLY SITE SPECIFIC FUNCTIONS HERE
+    ####################################
+
+    # Mesa Middle Corrections
+    if data == "MM_Table1_pandas_happy.dat":
+
+        # remove one anomalous pressure spike
+        output_file.BP_kPa_Avg.loc["2020-10-06 22:00" : "2020-10-06 23:00"] = np.nan
+
+    # Mesa East Corrections
+    if data == "ME_Table1_pandas_happy.dat":
+        # persistent pressure drop off
+        output_file['BP_kPa_Avg'].loc['2021-12-11':] = np.nan
+
+    # Mesa West Corrections
+    if data == "MW_Table1_pandas_happy.dat":
+        # pressure spike
+        output_file.BP_kPa_Avg.loc["2020-02-25 17:00" : "2020-02-26 00:00"] = np.nan
+        output_file.BP_kPa_Avg.loc["2018-04-13 20:00" : "2018-04-13 22:00"] = np.nan
+
+    ####################################
+    # END SITE SPECIFIC FUNCTIONS
+    ####################################
+
+    # now save it
+    outname = "_".join(data.split("_")[0:2]) + "_final_output.csv"
+    output_file.to_csv(outname, index_label='TIMESTAMP')
+ #   sys.exit()
 
 
 
